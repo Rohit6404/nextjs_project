d6a58efca413108e3bbf9410265c7da0216d6e88
Rohit Kumar Shaw <shawrk6404@gmail.com>
2024-12-17 03:27:48 -0700

    taking care of eslint warnings if needed.

---
+++ src/app/loading.js
@@ -0,0 +1,14 @@
+import  styles from "@/app/styles/common.module.css"
+const Loading = () => {
+    return (
+        <section className={styles.loading_section}>
+            <div className={styles.loading}>
+            </div>
+        </section>
+    );
+};
+
+export default Loading;
+
+
+// loading.io website ki help se loading animation bana skte h
